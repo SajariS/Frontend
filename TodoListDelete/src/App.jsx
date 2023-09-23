03516755709d9c7ec599a4575fe1dbaa8b5adb91
SajariS <santeri.sajari@gmail.com>
2023-09-23 15:13:43 +0300

    Lisätty todoListDelete

---
+++ TodoListDelete/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import TodoListDelete from "./TodoListDelete";
+
+function App() {
+  return(
+    <div classname="App">
+      <TodoListDelete />
+    </div>
+  );
+}
+
+export default App
