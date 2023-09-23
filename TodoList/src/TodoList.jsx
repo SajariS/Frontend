e88dda12d9e36ebf4c00bf25919692b31db9dfea
SajariS <santeri.sajari@gmail.com>
2023-09-23 15:00:10 +0300

    Todolist tehty

--- TodoList/src/TodoList.jsx
+++ TodoList/src/TodoList.jsx
@@ -1,25 +1,41 @@
 import { useState } from "react";
 
-const [input, setInput] = useState({desc: '', date: ''});
-const [todo, setTodo] = useState([]);
+function TodoList() {
+    const [input, setInput] = useState({desc: '', date: ''});
+    const [todo, setTodo] = useState([]);
 
-const inputChanged =(event) => {
-    setInput({...input, [event.target.name]: event.target.value});
+    const inputChanged =(event) => {
+        setInput({...input, [event.target.name]: event.target.value});
+    }
+
+    const addTodo = () => {
+        event.preventDefault();
+        setTodo([...todo, input]);
+    }
+
+    return(
+        <>
+            <form onSubmit={addTodo}>
+                <label for="desc">Description: </label>
+                <input type="text" name="desc" value={input.desc} onChange={inputChanged} id="desc"/>
+                <label for="date">Date: </label>
+                <input type="text" name="date" value={input.date} onChange={inputChanged} id="date"/>
+                <input type="submit" value="Add" />
+            </form>
+            {todo.length > 0 && <table>
+                <tbody>
+                    <tr>
+                        <th>Date</th><th>Description</th>
+                    </tr>
+                    {todo.map((todo, index) => (
+                        <tr key={index}>
+                            <td>{todo.date}</td>
+                            <td>{todo.desc}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table> }
+        </>
+    )
 }
-
-const addTodo = (event) => {
-    event.preventDefault();
-    setTodo([...todo, input]);
-}
-
-return(
-    <>
-        <form onSubmit={addTodo}>
-            <label for="desc">Description</label>
-            <input type="text" value={input.desc} onChange={inputChanged} id="desc"/>
-            <label for="date">Date</label>
-            <input type="text" value={input.date} onChange={inputChanged} id="date"/>
-            <input type="submit" value="Add Todo" />
-        </form>
-    </>
-)
+export default TodoList;
